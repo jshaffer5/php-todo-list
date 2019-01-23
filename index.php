0726bd49ffba015763e37bf33b8c9267c6f1e4f1
js <joshtwoone@gmail.com>
2019-01-22 23:38:38 -0800

    finished front end for item removal, need to create sql delete query

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
     <head>
         <title>my todos</title>
         <link rel="stylesheet" href="styles.css">
+        <script>
+        function removeItem(id){
+            // console.log(get);
+            element = document.getElementById(id);
+            nextElement = element.nextElementSibling;
+            previousElement = element.previousElementSibling;
+            //hide the title, description and "Remove" button
+            element.style.display = "none";
+            nextElement.style.display = "none";
+            previousElement.style.display = "none";
+
+        }
+        </script>
     </head>
     <body>
         <h1>Current List</h1>
@@ -21,8 +34,8 @@
                 $title = $row["todoTitle"];
                 $description= $row["todoDescription"];
                 $date = $row["date"];
-                echo $title . " - ";
-                echo "<a href=\"detail.php?id=$id\">" . $description . " </a><br>";
+                echo "<a>$title - </a>";
+                echo "<a id=\"$id\" href=\"detail.php?id=$id\">" . $description . " </a><button id=\"id\" value=\"$id\" onclick=\"removeItem(this.value)\">Remove</button><br>";
             }
         } else {
             echo "0 results";
